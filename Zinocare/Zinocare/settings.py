a827c62ee692d8a03d2d9d9277812bd686fd11b9
Kiproy04 <delleroy04@gmail.com>
2025-08-21 14:04:43 +0300

    Implemented different apps to hold logics

--- Zinocare/Zinocare/settings.py
+++ Zinocare/Zinocare/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts.apps.AccountsConfig',
+    'consultations.apps.ConsultationsConfig',
+    'livestock.apps.LivestockConfig',
+    'notifications.apps.NotificationsConfig',
+    'vaccinations.apps.VaccinationsConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
