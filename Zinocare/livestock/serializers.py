224013783f93c4e0aa41659105cb27353d9a6bbb
Kiproy04 <delleroy04@gmail.com>
2026-03-25 01:05:27 +0300

    Fix drf-spectacular schema generation errors

--- Zinocare/livestock/serializers.py
+++ Zinocare/livestock/serializers.py
@@ -1,10 +1,7 @@
 from rest_framework import serializers
 from .models import Animal
-from accounts.serializers import MkulimaProfileSerializer
 
 class AnimalSerializer(serializers.ModelSerializer):
-    mkulima = MkulimaProfileSerializer(read_only=True)
-
     class Meta:
         model = Animal
         fields = ["id", "name", "species", "mkulima", "breed", "sex", "date_of_birth", "created_at"]
