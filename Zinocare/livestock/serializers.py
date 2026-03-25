80ba4cac40c4f5ec16b244cad168d5fbad836775
Kiproy04 <delleroy04@gmail.com>
2026-03-25 18:15:50 +0300

    Fix mkulimaprofile attribute and AnimalSerializer read only

--- Zinocare/livestock/serializers.py
+++ Zinocare/livestock/serializers.py
@@ -5,3 +5,4 @@ class AnimalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Animal
         fields = ["id", "name", "species", "mkulima", "breed", "sex", "date_of_birth", "created_at"]
+        read_only_fields = ["id", "mkulima", "created_at"]  
