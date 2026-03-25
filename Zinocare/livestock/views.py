cfcfd4ed0dd490c76bd2ce988cf1a2adcc671aca
Kiproy04 <delleroy04@gmail.com>
2026-03-25 18:34:45 +0300

    Fix mkulimaprofile to catch error before performing create

--- Zinocare/livestock/views.py
+++ Zinocare/livestock/views.py
@@ -4,17 +4,19 @@ from rest_framework.response import Response
 from .models import Animal
 from .serializers import AnimalSerializer
 
-
-class AnimalDetailView(generics.RetrieveAPIView):
+class AnimalDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AnimalSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-        if user.role == "mkulima":
-            return Animal.objects.filter(mkulima=user.mkulimaprofile)
-        elif user.role == "vet":
-            return Animal.objects.all()
+        try:
+            if user.role == "mkulima":
+                return Animal.objects.filter(mkulima=user.mkulimaprofile)
+            elif user.role == "vet":
+                return Animal.objects.all()
+        except Exception:
+            pass
         return Animal.objects.none()
 
     def put(self, request, *args, **kwargs):
@@ -41,16 +43,20 @@ class AnimalDetailView(generics.RetrieveAPIView):
             )
         return self.destroy(request, *args, **kwargs)
 
+
 class AnimalListCreateView(generics.ListCreateAPIView):
     serializer_class = AnimalSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-        if user.role == "mkulima":
-            return Animal.objects.filter(mkulima=user.mkulimaprofile)
-        elif user.role == "vet":
-            return Animal.objects.all()  
+        try:
+            if user.role == "mkulima":
+                return Animal.objects.filter(mkulima=user.mkulimaprofile)
+            elif user.role == "vet":
+                return Animal.objects.all()
+        except Exception:
+            pass
         return Animal.objects.none()
 
     def create(self, request, *args, **kwargs):
@@ -62,4 +68,8 @@ class AnimalListCreateView(generics.ListCreateAPIView):
         return super().create(request, *args, **kwargs)
 
     def perform_create(self, serializer):
-        serializer.save(mkulima=self.request.user.mkulimaprofile)
+        try:
+            serializer.save(mkulima=self.request.user.mkulimaprofile)
+        except Exception as e:
+            from rest_framework.exceptions import ValidationError
+            raise ValidationError({"detail": f"Profile not found: {str(e)}"})
